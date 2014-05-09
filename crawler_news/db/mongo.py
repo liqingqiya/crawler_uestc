2f4fa8a6b61669447dab3b3b33bf42e20fc95497
liqing <liqing@wesharetech.com.cn>
2014-05-10 00:19:10 +0800

    增加数据库逻辑层的功能

--- crawler_news/db/mongo.py
+++ crawler_news/db/mongo.py
@@ -14,13 +14,32 @@ def insert_news(news_dict):
   logging.info("leaving insert_news...")
   return mongodb.insert_document("news", news_dict)
 
-def update_news(condition, news_dict):
-  pass
+def update_news_by_id(news_id, news_dict):
+  """
+  根据news的唯一id,更新文档
+  """
+  return update_document({"_id":news_id}, new_dict)
 
-def delete_news():
-  pass
+def delete_news_by_id(news_id):
+  """
+  根据id号删除文档
+  """
+  mongodb.delete_document({"_id":news_id})
 
-def find_news():
-  pass
+def delete_news_by_datetime(datetime):
+  """
+  根据时间,来删除新闻
+  """
+  mongodb.delete_document({"datetime":datetime})
 
+def find_news_by_datetime(datetime):
+  """
+  找到一定时间的新闻
+  """
+  return mongodb.find_document({"datetime":datetime})
 
+def find_news_by_id(news_id):
+  """
+  根据id查询news
+  """
+  return mongodb.find_document({"_id":news_id})
