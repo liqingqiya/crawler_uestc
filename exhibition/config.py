d29867614c99b70271813dbc838cf697562ca711
liqing <liqing@wesharetech.com.cn>
2014-05-22 11:09:52 +0800

    引进bootstrap框架

--- exhibition/config.py
+++ exhibition/config.py
@@ -11,6 +11,10 @@ TEMPLATE_PATH = "template/"
 #static
 STATIC_PATH = "static/"
 
+JS_URL = u"/static/bootstrap/js"
+CSS_URL = u"/static/bootstrap/css"
+IMG_URL = u"/static/bootstrap/img"
+
 #site path
 SITE_PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,3 +22,5 @@ SITE_PATH = os.path.dirname(os.path.abspath(__file__))
 LOG_PATH = os.path.join(SITE_PATH, "log")
 #log file
 LOG_FILE = os.path.join(LOG_PATH, "web.log")
+
+
--- remove_pyc_file.py
+++ remove_pyc_file.py
@@ -4,19 +4,21 @@
 import glob
 import os
 import sys
+import fnmatch
 
 root_path = os.path.dirname(os.path.abspath(__file__))
 
 def recurive_remove(path):
+  print "enter recurive_remove..."
+  print "path:%s"%path
   os.chdir(root_path)
+  for item in glob.glob("*.pyc"):
+    os.remove(item)
+
   for item in os.listdir(root_path):
+    print "item:%s"%item
     if os.path.isdir(item):
       recurive_remove(item)
-    else:
-      if fnmatch.fnmatch(item, "*.pyc"):
-        os.remove(item)
-        print "remove:%s"%item
-
   
 if __name__ == "__main__":
   recurive_remove(root_path)
